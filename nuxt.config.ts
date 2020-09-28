4dec7eb5b9c1ff73e4f7f77a60379bc9dce57a49
y-temp4 <y.temp4@gmail.com>
2020-09-28 19:42:28 +0900

    Setup typescript

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
-export default {
+import { NuxtConfig } from '@nuxt/types'
+
+const config: NuxtConfig = {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'nuxt-fb-tailwind-ts-template',
@@ -35,3 +37,5 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 }
+
+export default config
